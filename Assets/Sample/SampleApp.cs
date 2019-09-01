faa7fb808bead4d2aeabf816475aedc5bc11b8e8
aki517 <6293424+aki517@users.noreply.github.com>
2019-09-01 12:00:53 +0900

    Update project

---
+++ Assets/Sample/SampleApp.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Goisagi;
+
+/// <summary>
+/// 
+/// </summary>
+public class SampleApp : MonoBehaviour
+{
+    [SerializeField] FallThroughHandler fallThroughHandler = null;
+
+  // Start is called before the first frame update
+    void Start()
+    {
+        // コールバック登録.
+        FallThroughHandler fth = fallThroughHandler;
+        fth.onPress += OnPress;
+        fth.onBeginPress += OnBeginPress;
+        fth.onEndPress += OnEndPress;
+
+        fth.onDrag += OnDrag; 
+        fth.onBeginDrag += OnBeginDrag;
+        fth.onEndDrag += OnEndDrag;
+
+        fth.onPinch += OnPinch;
+        fth.onBeginPinch += OnBeginPinch;
+        fth.onEndPinch += OnEndPinch;
+
+        fth.onFlick += OnFlick;
+        fth.onDoubleClick += OnDoubleClick;
+    }
+
+    void OnDestroy()
+    {
+        // コールバック登録解除.
+        FallThroughHandler fth = fallThroughHandler;
+        fth.onPress -= OnPress;
+        fth.onBeginPress -= OnBeginPress;
+        fth.onEndPress -= OnEndPress;
+
+        fth.onDrag -= OnDrag; 
+        fth.onBeginDrag -= OnBeginDrag;
+        fth.onEndDrag -= OnEndDrag;
+
+        fth.onPinch -= OnPinch;
+        fth.onBeginPinch -= OnBeginPinch;
+        fth.onEndPinch -= OnEndPinch;
+
+        fth.onFlick -= OnFlick;
+        fth.onDoubleClick -= OnDoubleClick;
+    }
+
+    void OnPress( bool isDown ){ AddGUILog("OnPress"); }
+    void OnBeginPress( Vector2 pressPos ){ AddGUILog("OnBeginPress"); }
+    void OnEndPress( Vector2 releasePos ){ AddGUILog("OnEndPress"); }
+
+    void OnDrag( Vector2 delta, Vector2 dragPos ){ AddGUILog("OnDrag"); }
+    void OnBeginDrag( Vector2 dargPos ){ AddGUILog("OnBeginDrag"); }
+    void OnEndDrag( Vector2 releasePos ){ AddGUILog("OnEndDrag"); }
+
+    void OnPinch( Vector2 beginPos, Vector2 endPos, float pinchRange ){ AddGUILog("OnPinch"); }
+    void OnBeginPinch( Vector2 beginPos, Vector2 endPos ){ AddGUILog("OnBeginPinch"); }
+    void OnEndPinch(){ AddGUILog("OnEndPinch"); }
+
+    void OnFlick( Vector2 releasePos, Vector2 delta, FlickDirType flickType ){ AddGUILog("OnFlick"); }
+    void OnDoubleClick( Vector2 releasePos ){ AddGUILog("OnDoubleClick"); }
+
+
+#region DEBUG_GUI_LOG
+    const int DISPLAY_LOG_MAX = 30;
+    List<string> logList = new List<string>();
+    GUIStyle guiStyle = new GUIStyle();
+
+    void OnGUI()
+    {
+        guiStyle.fontSize = ( int )(Screen.height / (DISPLAY_LOG_MAX * 2));
+        foreach( var item in logList ){
+            GUILayout.Label( item, guiStyle );
+        }
+    }
+
+    void AddGUILog( string text )
+    {
+        if( logList.Count > DISPLAY_LOG_MAX ){
+            logList.RemoveAt( 0 );
+        }
+
+        logList.Add( text );
+    }
+#endregion // DEBUG_GUI_LOG
+
+
+}   // End of class SampleApp.
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # fall-through-handler
-UIボタン以外のタッチ制御を行うクラス
+UI以外のタッチ制御を行うクラス
+
+### Unity Version
+Unity 2019.1.0f2
+
+### 使い方
+1. Hierarchyに適当なGameObjectを作成  
+2. GameObjectにFallThroughHandlerコンポーネントを追加  
+3. Inspectorの各パラメータを設定(詳細は後述)  
+4. FallThroughHandlerへ受け取りたい入力処理に応じたコールバックを設定  
+
+### パラメータ
+
+|名前|説明|  
+|:---|:---|  
+|TargetCanvas|対象Canvas,シーン再生時にこのCanvasの子要素になる|  
+|FlickDetectDistance|フリック入力扱いにする移動量|  
+|DoubleClickInterval|ダブルクリックの受付間隔(秒)|  
+
+### 補足
+- 対象CanvasにGraphicRaycasterが無い場合はダミーを追加
+- UI関連の入力制御を妨げないために子要素追加時に transform.SetAsFirstSibling() を使い描画順を最奥に設定
+- コールバックの設定方法は SampleApp.cs 内に記述
